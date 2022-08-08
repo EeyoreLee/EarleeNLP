4fba2bc1ebfb8d3e4ddffd727032a26801aa1e39
EeyoreLee <li.chunyu0412@outlook.com>
2022-08-08 15:28:32 +0800

    Move unsupported models into trash

---
+++ models/simcse/argument_simcse.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+'''
+@create_time: 2022/06/24 10:45:09
+@author: lichunyu
+'''
+from dataclasses import dataclass, field
+from typing import Dict
+
+
+@dataclass
+class ModelArgument:
+
+    author: str = field(default="EeyoreLee", metadata={"help": "author"})
+    model_init_param: dict = field(default_factory=dict, metadata={"help": "param for model's initialization"})
+    collection_param: dict = field(default_factory=dict, metadata={"help": "param for collection's initialization"})
